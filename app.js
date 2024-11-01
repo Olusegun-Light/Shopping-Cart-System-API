5d0ee2f11620f5e2107fb3f37a2b1aa37da9f723
Olusegun-Light <lightolusegun1@gmail.com>
2024-11-01 18:15:49 +0100

    added root route

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Welcome To Shopping Cart API!");
+});
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/user", userRoutes);
